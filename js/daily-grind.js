7e8490b7c7d6902e26970bbd1185d76c48e06d07
Replit user <>
2023-06-05 20:39:35 +0000

    add text

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -12,6 +12,16 @@
 
 */
 
+function coffeeTemplate(coffee){
+return   `
+<p>
+    		 <img src="${coffee.pic}" alt="${coffee.alt}"id="coffee" />
+            <strong class="feature">${coffee.day}'s Coffee Special:</strong>${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>,${coffee.desc}</p>
+
+`;
+
+  
+}
 
 let myDate =new Date();
 
@@ -20,6 +30,19 @@ let today =myDate.getDay();
 //today =3;
 
 switch(today){
+
+     case 1:
+          today ="Monday";
+          coffee = {
+            name:"Bubble Tea", 
+            color: "pink",
+            pic: "images/bubble-tea.jpg",
+            alt: "A picture of a bubble tea",
+            day: "Monday",
+            desc: `I like me some bubble tea!`
+          };
+
+     break;
 
      case 2:
           today ="Tuesday";
@@ -31,18 +54,12 @@ switch(today){
 
      default:
          today ="Not Sure What Day It Is!";
-
-}
-coffee = {
-    name:"Bubble Tea", 
-    color: "pink",
-    pic: "images/bubble-tea.jpg",
-    alt: "A picture of a bubble tea",
-    day: "Wednesday",
-    desc: `I like me some bubble tea!`
-
+    alert("falling into alert!");
 
 }
 
+alert(coffeeTemplate(coffee))
+
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 //alert(today);
 console.log(coffee);
