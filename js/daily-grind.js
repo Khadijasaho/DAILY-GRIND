fa81d4953276518b54a0edb6f2a54ffaf5b414ba
Khadija Saho <sahokhadija444@gmail.com>
2023-06-06 14:00:11 -0700

    add text

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -19,15 +19,31 @@ return   `
             <strong class="feature">${coffee.day}'s Coffee Special:</strong>${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>,${coffee.desc}</p>
 
 `;
-
   
 }
 
 let myDate =new Date();
-
 let today =myDate.getDay();
+let coffe = "";
+
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate query string parameters
+const urlParams = new URLSearchParams(queryString);
+
 
 //today =3;
+
+if(urlParams.has("day")){//from querystring
+    today = urlParams.get("day");
+ }
+ 
+ today = parseInt(today);
+ console.log (today)
 
 switch(today){
 
@@ -46,11 +62,87 @@ switch(today){
 
      case 2:
           today ="Tuesday";
+          coffee = {
+            name:"Drip", 
+            color: "black",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip coffee",
+            day: "Tuesday",
+            desc: `Give it to me straight!`
+          };
      break;
 
      case 3:
         today ="Wednesday";
+        coffee = {
+            name:"Caramel Latte", 
+            color: "brown",
+            pic: "images/caramel-latte.jpg",
+            alt: "A picture of a caramel latte coffee",
+            day: "Wednesday",
+            desc: `Best latte ever!`
+          };
+
+        
    break;
+
+   case 4:
+        today ="Thursday";
+        coffee = {
+            name:"Mocha", 
+            color: "red",
+            pic: "images/mocha.jpg",
+            alt: "A picture of a mocha coffee",
+            day: "Thursday",
+            desc: `Chocolate Coffee!`
+          };
+
+        
+   break;
+
+   case 5:
+        today ="Friday";
+        coffee = {
+            name:"Cold Brew", 
+            color: "blue",
+            pic: "images/cold-brew.jpg",
+            alt: "A picture of a cold brew coffee",
+            day: "Friday",
+            desc: `Nothing Better Than a Cold Brew On a Hot Day!`
+          };
+
+        
+   break;
+
+   case 6:
+    today ="Saturday";
+    coffee = {
+        name:" Frappaccino", 
+        color: "purple",
+        pic: "images/frappaccino.jpg",
+        alt: "A picture of a frappaccino coffee",
+        day: "Saturday",
+        desc: `Coldest Coffee Ever`
+      };
+
+    
+break;
+
+case 7:
+    today ="Sunday";
+    coffee = {
+        name:"pumpkin-spice-latte", 
+        color: "orange",
+        pic: "images/pumpkin-spice-latte.jpg",
+        alt: "A picture of a pumpkin spice latte coffee",
+        day: "Sunday",
+        desc: ` Most popular coffee during fall!`
+      };
+
+    
+break;
+
+
 
      default:
          today ="Not Sure What Day It Is!";
@@ -61,5 +153,6 @@ switch(today){
 alert(coffeeTemplate(coffee))
 
 document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+document.querySelector("html").style.background = coffee.color;
 //alert(today);
 console.log(coffee);
